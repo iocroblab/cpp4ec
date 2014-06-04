5a4ee063c091d7ca695f7fbde1215c57be69b1cc
sergi ruiz <sergi006@hotmail.com>
2014-06-04 14:36:57 +0200

    Added the src code of NRT thread

---
+++ src/EcNRTThread/EcNRTThread.h
@@ -0,0 +1,19 @@
+#ifndef _nrealtimetask_
+#define _nrealtimetask_
+
+#include <soem/ethercattype.h>
+#include <soem/nicdrv.h>
+#include <soem/ethercatbase.h>
+#include <soem/ethercatmain.h>
+#include <soem/ethercatconfig.h>
+#include <soem/ethercatdc.h>
+#include <soem/ethercatcoe.h>
+#include <soem/ethercatprint.h>
+
+
+//int NRTtaskFinished;
+void nrt_thread(void *unused);
+
+
+
+#endif
